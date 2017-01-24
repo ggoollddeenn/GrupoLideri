0c668f9752c27dea6be652a5e7688044806278b6
raulb <raulb@LAPTOP-4UN72CN9>
2017-01-23 18:48:20 -0600

    Primer Commit

---
+++ Modelo/ServiceObject/SO_Folio.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo.ServiceObject
+{
+    public class SO_Folio
+    {
+        /// <summary>
+        /// Método que inserta un objeto de tipo TBL_Folios en la tabla.
+        /// </summary>
+        /// <param name="folio">Objeto de tipo TBL_Folios que se quiere insertar.</param>
+        /// <returns>Retorna un true si se ejecutó de manera correcta, un false si hubo algun problema.</returns>
+        public bool InsertFolio(TBL_FOLIOS folio)
+        {
+            try
+            {
+                //Inicializamos la conexión a través de EntityFramework.
+                using (var Contexto = new BD_JDAEntities())
+                {
+                    //Agregamos el objeto a la tabla TBL_FOLIOS.
+                    Contexto.TBL_FOLIOS.Add(folio);
+
+                    //Guardamos los cambios y el resultado nos indicará el numero de elementos afectados en la base de datos.
+                    int registros = Contexto.SaveChanges();
+
+                    //Retornamos el valor resultante de la comparanción.
+                    return registros > 0;
+                }
+            }
+            catch (Exception er)
+            {
+                // Registrar error en BD  con er.InnerException.ToString();
+                return false;
+            }
+                
+        }
+    }
+}
---
+++ Modelo/ServiceObject/SO_Persona.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo.ServiceObject
+{
+    public class SO_Persona
+    {
+        /// <summary>
+        /// Método que busca algún registro que conincida con el usuario y la contraseña recibida en el parámetro.
+        /// </summary>
+        /// <param name="usuario">Cadena que representa el usuario que se requiere buscar.</param>
+        /// <param name="contrasena">Cadena que representa la contraseña que se requiere buscar.</param>
+        /// <returns>Retorna un true si existe un registro con el usuario y la contraseña enviadas a los parámetros, de lo contrario renorna un false.</returns>
+        public bool Login(string usuario, string contrasena)
+        {
+            try
+            {
+                //Inicializamos la conexión a la base de datos a través de EntityFramework.
+                using (var Contexto = new BD_JDAEntities())
+                {
+                    //Realizamos la consulta.
+                    int registros = (from p in Contexto.TBL_PERSONA
+                                     where p.CONTRASENA == contrasena && p.USUARIO == usuario
+                                     select p).ToList().Count;
+
+                    //Retornamos el valor resultante de la comparación.
+                    return registros > 0;
+                }
+            }
+            catch (Exception er)
+            {
+                //Registrar error en base de datos.
+                return false;
+            }
+        }
+    }
+}
